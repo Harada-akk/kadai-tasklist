f5d7f6b18a05e4ff0629c22c5ba6074046fbfd1a
AK <akiko-harada@x-power.co.jp>
2020-02-17 15:07:36 +0900

    Modify Index/NewServlet

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -42,6 +43,12 @@ public class IndexServlet extends HttpServlet {
         response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        // 9.2 indexのビューを作成
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
